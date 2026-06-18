91d4c315fb0c3da8d80523389f5eab54672f9400
anshi-4 <anshikarathi87574@gmail.com>
2026-06-18 18:22:08 +0530

    Production deployment update

--- backend/app.py
+++ backend/app.py
@@ -108,4 +108,4 @@ def decrypt():
 if __name__ == '__main__':
     # Flask runs on port 5000 by default
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
